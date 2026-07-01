184b868cad155cb961d929704dbc976811d1b84e
Grikxx <sgsparshking@gmail.com>
2026-07-01 22:43:57 +0530

    Some Changes

--- app/components/layout/navbar.tsx
+++ app/components/layout/navbar.tsx
@@ -16,7 +16,7 @@ import { cn } from "@/lib/utils";
 import { ArrowUpRight, TextAlignJustify } from "lucide-react";
 import { useCallback, useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
-
+import Link from "next/link";
 export type NavigationSection = {
   title: string;
   href: string;
@@ -34,10 +34,6 @@ const navigationData: NavigationSection[] = [
   {
     title: "Projects",
     href: "/projects",
-  },
-  {
-    title: "TechStack",
-    href: "/techstack",
   },
   {
     title: "About me",
@@ -103,12 +99,12 @@ const Navbar = () => {
       <DropdownMenuContent align="end" className="w-56 mt-2">
         {navigationData.map((item) => (
           <DropdownMenuItem key={item.title}>
-            <a
+            <Link
               href={item.href}
               className="w-full cursor-pointer text-sm font-medium"
             >
               {item.title}
-            </a>
+            </Link>
           </DropdownMenuItem>
         ))}
       </DropdownMenuContent>
@@ -141,23 +137,23 @@ const Navbar = () => {
             className={cn(
               "relative left-8 z-10  ",
               isOpen
-                ? "transition-all  duration-500 left-[calc(100%-72px)] "
-                : "transition-all  duration-500 group-hover:left-[calc(100%-72px)] ",
+                ? "transition-all  duration-500 left-[calc(100%-100px)] "
+                : "transition-all  duration-500 group-hover:left-[calc(100%-100px)] ",
             )}
           >
-            Grikxx
+            Navigation
           </span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-56 mt-2">
         {navigationData.map((item) => (
           <DropdownMenuItem key={item.title}>
-            <a
+            <Link
               href={item.href}
               className="w-full cursor-pointer text-sm font-medium"
             >
               {item.title}
-            </a>
+            </Link>
           </DropdownMenuItem>
         ))}
       </DropdownMenuContent>
@@ -167,7 +163,7 @@ const Navbar = () => {
   return (
     <div>
       <header className="bg-background ">
-        <div className="max-w-7xl mx-auto w-full px-4 py-4 sm:px-6">
+        <div className="max-w-9xl mx-auto w-full px-4 py-4 sm:px-6">
           <nav
             className={cn(
               "w-full flex items-center h-fit justify-between gap-3.5 md:gap-6 transition-all duration-500",
@@ -176,23 +172,58 @@ const Navbar = () => {
                 : "bg-transparent border-transparent",
             )}
           >
-            <div className="flex justify-start h-fit items-center gap-4">
-              <div className="md:hidden">
-                <ProfileButton />
-              </div>
-
-              <a href="#" className="hidden md:block">
-                <h1>Grikxx</h1>
-              </a>
+            <div className="flex justify-start h-fit items-center gap-4 md:hidden">
+              <ProfileButton />
+            </div>
+            <div className=" h-fit items-center ">
+              <Link href="/" className="">
+                <svg 
+                width="66.000000pt" height="45.000000pt" viewBox="0 0 600.000000 326.000000"
+                preserveAspectRatio="xMidYMid meet">
+                <g transform="translate(0.000000,326.000000) scale(0.066667,-0.066667)"
+                fill="#000000" stroke="none">
+                <path d="M2019 4138 c-993 -201 -1582 -2303 -763 -2718 280 -142 673 -37 862
+                231 105 149 107 132 17 -195 -123 -452 -259 -595 -588 -616 -204 -13 -256 -78
+                -156 -196 167 -199 579 -143 806 108 166 185 225 369 338 1047 26 152 64 358
+                85 457 51 239 59 352 30 392 -77 103 -623 -49 -726 -202 -100 -148 2 -225 236
+                -181 105 20 105 23 14 -138 -287 -502 -648 -555 -767 -112 -193 718 266 1787
+                799 1862 258 35 262 -374 6 -653 -117 -126 -87 -223 75 -247 258 -39 459 235
+                463 631 3 405 -292 620 -731 530z"/>
+                <path d="M4914 3836 c-41 -36 -35 -8 -242 -1238 -80 -474 -170 -688 -349 -829
+                -183 -144 -182 83 4 743 107 379 95 429 -114 441 -151 8 -177 -22 -221 -260
+                -19 -99 -47 -251 -64 -338 -56 -296 -258 -630 -382 -630 -89 0 -78 92 60 523
+                160 496 141 546 -208 549 l-195 2 5 85 c11 187 -187 241 -317 88 -75 -89 -72
+                -206 8 -297 l46 -52 -24 -70 c-12 -39 -61 -162 -107 -273 -89 -216 -97 -254
+                -60 -291 81 -82 145 -8 306 357 l98 220 82 -9 c104 -12 100 -4 56 -115 -141
+                -352 -197 -723 -128 -851 126 -235 475 -156 645 145 58 102 64 100 65 -22 2
+                -332 368 -335 593 -5 55 80 84 98 68 42 -61 -223 20 -345 198 -297 87 24 103
+                57 131 271 35 278 77 436 153 590 140 279 407 480 399 300 -5 -122 -99 -239
+                -310 -386 -141 -100 -159 -165 -99 -364 187 -613 757 -651 1177 -79 67 91 77
+                89 118 -24 184 -508 698 -474 1099 72 67 91 66 91 106 -40 127 -419 481 -479
+                746 -126 170 225 94 368 -89 169 -166 -180 -258 -121 -398 252 l-32 86 102
+                116 c440 502 521 696 290 693 -120 -1 -148 -24 -231 -186 -96 -189 -203 -368
+                -220 -368 -15 0 -17 9 -50 203 -52 305 -78 343 -231 343 -133 1 -193 -30 -207
+                -106 -10 -52 131 -596 177 -684 31 -61 -324 -420 -463 -467 -130 -44 -199 21
+                -320 305 l-64 149 133 160 c442 531 496 702 208 659 -94 -14 -115 -37 -203
+                -216 -82 -168 -200 -361 -221 -361 -7 0 -21 64 -31 142 -41 313 -86 406 -201
+                421 -303 38 -335 -199 -95 -706 l67 -141 -107 -129 c-314 -378 -529 -393 -722
+                -48 -68 121 -68 118 17 160 338 163 515 483 397 716 -130 254 -444 177 -672
+                -167 -30 -45 -60 -80 -65 -76 -6 4 43 271 108 595 66 330 114 603 109 623 -18
+                73 -257 96 -329 31z"/>
+                <path d="M4278 3585 c-168 -40 -233 -257 -111 -378 124 -125 363 -58 406 113
+                46 184 -97 312 -295 265z"/>
+                </g>
+                </svg>
+              </Link>
             </div>
             <div>
-              <NavigationMenu className="max-md:hidden bg-muted p-0.5 rounded-full">
+              <NavigationMenu className="max-md:hidden bg-muted p-0.5 rounded-full py-1">
                 <NavigationMenuList className="flex gap-0">
                   {navigationData.map((navItem) => {
                     const isActive = pathname === navItem.href;
                     return (
                       <NavigationMenuItem key={navItem.title}>
-                        <NavigationMenuLink
+                        <Link
                           href={navItem.href}
                           className={`px-2 md:px-4 py-2 text-sm font-medium rounded-full transition tracking-normal outline ${
                             isActive
@@ -201,14 +232,16 @@ const Navbar = () => {
                           }`}
                         >
                           {navItem.title}
-                        </NavigationMenuLink>
+                        </Link>
                       </NavigationMenuItem>
                     );
                   })}
                 </NavigationMenuList>
               </NavigationMenu>
             </div>
+            <div>
             <MenusButton className=" md:flex" />
+            </div>
           </nav>
         </div>
       </header>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
 import Navbar from "./components/layout/navbar";
-
 export default function Home() {
   return (
-    <div>
-      Hellow        
+    <div className="flex justify-between bg-primary/10 mx-10 rounded-3xl py-10 px-10">
+      <div>
+        <p className="from-primary/40 to-primary bg-linear-to-r bg-clip-text text-4xl font-semibold text-transparent">
+          Hello
+        </p>
+        <p className="text-lg text-foreground">
+          Welcome to my portfolio webapp! Explore my projects and skills.
+        </p>
+      </div>
+      <div>
+        <img
+          src= '/pfp.jpg'
+          draggable="false"
+          alt="pfp"
+          className="w-50 h-auto object-cover rounded-full "
+        />
+      </div>
+  
     </div>
   );
 }
