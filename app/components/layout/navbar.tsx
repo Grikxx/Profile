034d1363948743a7da0ffc58d044ec279a4e9536
Grikxx <sgsparshking@gmail.com>
2026-07-01 18:39:05 +0530

    Fommated Text

--- app/components/layout/navbar.tsx
+++ app/components/layout/navbar.tsx
@@ -1,7 +1,17 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from "@/components/ui/navigation-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
 import { cn } from "@/lib/utils";
 import { ArrowUpRight, TextAlignJustify } from "lucide-react";
 import { useCallback, useEffect, useState } from "react";
@@ -61,57 +71,98 @@ const Navbar = () => {
     };
   }, [handleScroll, handleResize]);
 
-const MenusButton = ({ className }: { className?: string }) => (
-  <DropdownMenu open={clicked} onOpenChange={setClicked}>
-  <DropdownMenuTrigger asChild>
-  <Button className={cn('relative text-sm font-medium rounded-full h-10 p-1 ps-4 pe-12 group bg-primary  transition-all duration-500  ',clicked?"ps-12 pe-4 w-fit overflow-hidden  bg-primary/80":"transition-all duration-500 hover:ps-12 hover:pe-4 w-fit overflow-hidden hover:bg-primary/80", className)} >
-    <div className={cn('absolute right-1 w-8 h-8 bg-background text-foreground rounded-full flex items-center justify-center transition-all duration-500  ',clicked?" right-[calc(100%-36px)] rotate-90":" transition-all duration-500 group-hover:right-[calc(100%-36px)] group-hover:rotate-90")}>
-        <ArrowUpRight size={20} />
-    </div>
-    <span className={cn('relative  z-10   transition-all duration-500  ')}>
-      Menus
-    </span>
-    
-  </Button>
-  </DropdownMenuTrigger>
-  <DropdownMenuContent
-                  align="end"
-                  className="w-56 mt-2"
-                >
-                  {navigationData.map((item) => (
-                    <DropdownMenuItem key={item.title}>
-                      <a href={item.href} className="w-full cursor-pointer text-sm font-medium">{item.title}</a>
-                    </DropdownMenuItem>
-                  ))}
-    </DropdownMenuContent>
-  </DropdownMenu>
-);
+  const MenusButton = ({ className }: { className?: string }) => (
+    <DropdownMenu open={clicked} onOpenChange={setClicked}>
+      <DropdownMenuTrigger asChild>
+        <Button
+          className={cn(
+            "relative text-sm font-medium rounded-full h-10 p-1 ps-4 pe-12 group bg-primary  transition-all duration-500  ",
+            clicked
+              ? "ps-12 pe-4 w-fit overflow-hidden  bg-primary/80"
+              : "transition-all duration-500 hover:ps-12 hover:pe-4 w-fit overflow-hidden hover:bg-primary/80",
+            className,
+          )}
+        >
+          <div
+            className={cn(
+              "absolute right-1 w-8 h-8 bg-background text-foreground rounded-full flex items-center justify-center transition-all duration-500  ",
+              clicked
+                ? " right-[calc(100%-36px)] rotate-90"
+                : " transition-all duration-500 group-hover:right-[calc(100%-36px)] group-hover:rotate-90",
+            )}
+          >
+            <ArrowUpRight size={20} />
+          </div>
+          <span
+            className={cn("relative  z-10   transition-all duration-500  ")}
+          >
+            Menus
+          </span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56 mt-2">
+        {navigationData.map((item) => (
+          <DropdownMenuItem key={item.title}>
+            <a
+              href={item.href}
+              className="w-full cursor-pointer text-sm font-medium"
+            >
+              {item.title}
+            </a>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 
-const ProfileButton = ({ className }: { className?: string }) => (
-  <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
-  <DropdownMenuTrigger asChild>
-  <Button className={cn("relative text-sm font-medium rounded-full h-10 p-1 ps-4 pe-12 group bg-primary ",isOpen?"ps-12 pe-4 w-fit overflow-hidden transition-all duration-500 bg-primary/80":"transition-all duration-500 hover:ps-12 hover:pe-4 w-fit overflow-hidden hover:bg-primary/80", className)} >
-    <div className={cn("absolute left-1 w-8 h-8 bg-background text-foreground rounded-full flex items-center justify-center ",isOpen?"transition-all duration-500 left-[calc(100%-36px)] rotate-90":" transition-all duration-500 group-hover:left-[calc(100%-36px)] group-hover:rotate-90")}>
-        <TextAlignJustify size={20} />
-    </div>
-    <span className={cn("relative left-8 z-10  ",isOpen?"transition-all  duration-500 left-[calc(100%-72px)] ":"transition-all  duration-500 group-hover:left-[calc(100%-72px)] ")}>
-      Grikxx 
-    </span>
-    
-  </Button>
-  </DropdownMenuTrigger>
-  <DropdownMenuContent
-                  align="end"
-                  className="w-56 mt-2"
-                >
-                  {navigationData.map((item) => (
-                    <DropdownMenuItem key={item.title}>
-                      <a href={item.href} className="w-full cursor-pointer text-sm font-medium">{item.title}</a>
-                    </DropdownMenuItem>
-                  ))}
-    </DropdownMenuContent>
-  </DropdownMenu>
-);
+  const ProfileButton = ({ className }: { className?: string }) => (
+    <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
+      <DropdownMenuTrigger asChild>
+        <Button
+          className={cn(
+            "relative text-sm font-medium rounded-full h-10 p-1 ps-4 pe-12 group bg-primary ",
+            isOpen
+              ? "ps-12 pe-4 w-fit overflow-hidden transition-all duration-500 bg-primary/80"
+              : "transition-all duration-500 hover:ps-12 hover:pe-4 w-fit overflow-hidden hover:bg-primary/80",
+            className,
+          )}
+        >
+          <div
+            className={cn(
+              "absolute left-1 w-8 h-8 bg-background text-foreground rounded-full flex items-center justify-center ",
+              isOpen
+                ? "transition-all duration-500 left-[calc(100%-36px)] rotate-90"
+                : " transition-all duration-500 group-hover:left-[calc(100%-36px)] group-hover:rotate-90",
+            )}
+          >
+            <TextAlignJustify size={20} />
+          </div>
+          <span
+            className={cn(
+              "relative left-8 z-10  ",
+              isOpen
+                ? "transition-all  duration-500 left-[calc(100%-72px)] "
+                : "transition-all  duration-500 group-hover:left-[calc(100%-72px)] ",
+            )}
+          >
+            Grikxx
+          </span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56 mt-2">
+        {navigationData.map((item) => (
+          <DropdownMenuItem key={item.title}>
+            <a
+              href={item.href}
+              className="w-full cursor-pointer text-sm font-medium"
+            >
+              {item.title}
+            </a>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
   const pathname = usePathname();
   return (
     <div>
@@ -126,38 +177,38 @@ const ProfileButton = ({ className }: { className?: string }) => (
             )}
           >
             <div className="flex justify-start h-fit items-center gap-4">
-            <div className="md:hidden">
+              <div className="md:hidden">
                 <ProfileButton />
-            </div>
-            
-            <a href="#" className="hidden md:block">
-              <h1 >Grikxx</h1>
-            </a>
-              
+              </div>
+
+              <a href="#" className="hidden md:block">
+                <h1>Grikxx</h1>
+              </a>
             </div>
             <div>
               <NavigationMenu className="max-md:hidden bg-muted p-0.5 rounded-full">
                 <NavigationMenuList className="flex gap-0">
-                    {navigationData.map((navItem) => {
-                      const isActive = pathname === navItem.href;
-                        return (
-                          <NavigationMenuItem key={navItem.title}>
-                            <NavigationMenuLink
-                              href={navItem.href}
-                              className={`px-2 md:px-4 py-2 text-sm font-medium rounded-full transition tracking-normal outline ${
-                                isActive
-                                  ? "text-foreground bg-background outline-border shadow-xs" 
-                                  : "text-muted-foreground hover:text-foreground hover:bg-background outline-transparent hover:outline-border hover:shadow-xs" 
-                              }`}
-                            >
-                              {navItem.title}
-                            </NavigationMenuLink>
-                          </NavigationMenuItem>
-                        );})}
+                  {navigationData.map((navItem) => {
+                    const isActive = pathname === navItem.href;
+                    return (
+                      <NavigationMenuItem key={navItem.title}>
+                        <NavigationMenuLink
+                          href={navItem.href}
+                          className={`px-2 md:px-4 py-2 text-sm font-medium rounded-full transition tracking-normal outline ${
+                            isActive
+                              ? "text-foreground bg-background outline-border shadow-xs"
+                              : "text-muted-foreground hover:text-foreground hover:bg-background outline-transparent hover:outline-border hover:shadow-xs"
+                          }`}
+                        >
+                          {navItem.title}
+                        </NavigationMenuLink>
+                      </NavigationMenuItem>
+                    );
+                  })}
                 </NavigationMenuList>
               </NavigationMenu>
             </div>
-            <MenusButton className=" md:flex" /> 
+            <MenusButton className=" md:flex" />
           </nav>
         </div>
       </header>
