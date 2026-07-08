1ab5e60a2b9609826eed706847f78e83a26500ff
Grikxx <sgsparshking@gmail.com>
2026-07-08 10:04:09 +0530

    Test for Slider

--- app/components/layout/slider.tsx
+++ app/components/layout/slider.tsx
@@ -3,19 +3,70 @@
 import InfiniteSlider from "@/components/smoothui/infinite-slider";
 
 
-const technologies = 10;
+
+const tech = [
+  {
+    name: "React",
+    href: "/logos/React.svg",
+  },
+    {
+    name: "Tailwind CSS",
+    href: "/logos/Tailwind CSS.svg",
+  },
+  {
+    name: "Next.js",
+    href: "/logos/next.svg",
+  },
+  {
+    name: "CSS3",
+    href: "/logos/CSS3.svg",
+  },
+  {
+    name: "HTML5",
+    href: "/logos/HTML5.svg",
+  },
+  {
+    name: "Arch Linux",
+    href: "/logos/Arch Linux.svg",
+  },
+  {
+    name: "Express.js",
+    href: "/logos/Express.svg",
+  },
+  {
+    name: "Fedora",
+    href: "/logos/Fedora.svg",
+  },
+  {
+    name: "PostgresSQL",
+    href: "/logos/PostgresSQL.svg",
+  },
+  {
+    name: "JavaScript",
+    href: "/logos/JavaScript.svg",
+  },
+  {
+    name: "TypeScript",
+    href: "/logos/TypeScript.svg",
+  },
+  {
+    name: "Node.js",
+    href: "/logos/Node.js.svg",
+  }
+]
+
 const InfiniteSliderDemo = () => {
   return (
-    <div className="flex min-h-[200px] flex-col items-center justify-center gap-8">
+    <div className="flex w-full min-h-[200px] flex-col items-center justify-center gap-2">
       {/* Basic horizontal slider */}
       <div className="w-full max-w-md">
-        <InfiniteSlider gap={16} speed={50}>
-          {Array.from({ length: technologies }, (_, i) => (
+        <InfiniteSlider gap={16} speed={70}>
+          {Array.from({ length: tech.length }, (_, i) => (
             <div
               className="flex h-20 w-20 shrink-0 items-center justify-center rounded-lg bg-muted text-foreground"
               key={`basic-${i + 1}`}
             >
-              {i + 1}vagabond 
+              <img src={tech[i].href} alt={tech[i].name} className="h-12 w-12" draggable="false" />
             </div>
           ))}
         </InfiniteSlider>
@@ -24,12 +75,12 @@ const InfiniteSliderDemo = () => {
       {/* Slider with hover pause */}
       <div className="w-full max-w-md">
         <InfiniteSlider gap={16} speed={50} speedOnHover={0}>
-          {Array.from({ length: technologies }, (_, i) => (
+          {Array.from({ length: tech.length }, (_, i) => (
             <div
-              className="flex h-20 w-20 shrink-0 items-center justify-center rounded-lg bg-brand text-white"
+              className="flex h-20 w-20 shrink-0 items-center justify-center rounded-lg bg-brand text-foreground"
               key={`hover-${i + 1}`}
             >
-              {i + 1}
+              <img src={tech[i].href} alt={tech[i].name} className="h-12 w-12" draggable="false"/>
             </div>
           ))}
         </InfiniteSlider>
@@ -37,13 +88,13 @@ const InfiniteSliderDemo = () => {
 
       {/* Reverse direction */}
       <div className="w-full max-w-md">
-        <InfiniteSlider gap={16} reverse speed={50}>
-          {Array.from({ length: 5 }, (_, i) => (
+        <InfiniteSlider gap={16} reverse speed={70}>
+          {Array.from({ length: tech.length }, (_, i) => (
             <div
               className="flex h-20 w-20 shrink-0 items-center justify-center rounded-lg bg-muted text-foreground"
               key={`reverse-${i + 1}`}
             >
-              {i + 1}
+              <img src={tech[i].href} alt={tech[i].name} className="h-12 w-12" draggable="false"/>
             </div>
           ))}
         </InfiniteSlider>
